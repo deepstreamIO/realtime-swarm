eb2f09caaaf14a7912bed2b623bf71bfb352472f
Wolfram Hempel <hempel.wolfram@gmail.com>
2016-10-16 19:59:39 +0200

    presentation so far

--- presentation/js/enums.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-	EMPTY: 0,
-	ANT: 1,
-	FOOD: 2,
-	OBSTACLE: 3,
-	ANT_HILL: 4
-}
--- presentation/js/index.js
+++ presentation/js/index.js
@@ -1,23 +1,12 @@
-const World = require( './world' );
-const Renderer = require( './canvas-2d-renderer' );
-
-const WIDTH = 200;
-const HEIGHT = 150;
-const world = new World({
-    width: WIDTH,
-    height: HEIGHT,
-    maxAnts: 150,
-    antHill: { x: 60, y: 60 }
-});
-const renderer = new Renderer( WIDTH, HEIGHT, 4 );
-
-function tick() {
-    world.update();
-    renderer.render( world.data );
-    requestAnimationFrame( tick );
-}
 
 window.onload = function() {
-    document.body.appendChild( renderer.element );
-    tick();
+	impress().init();
+
+	var rootElement = document.getElementById( "impress" );
+	rootElement.addEventListener( "impress:stepenter", function() {
+		var currentStep = document.querySelector( ".present" );
+		if( currentStep.id === 'swarm-a' ) {
+			document.querySelector( '.swarm-a' ).style.display = 'block';
+		}
+	});
 }
