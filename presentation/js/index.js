1996465d92b5d13e2fad20a7a9bc78375c4f2a48
Wolfram Hempel <wolfram.hempel@hoxton-one.com>
2016-10-17 15:29:07 +0200

    presentation almost done

--- presentation/js/index.js
+++ presentation/js/index.js
@@ -1,10 +1,15 @@
 var BeeSwarm = require( '../../bees/js/' );
 
 window.onload = function() {
+	var slides = document.querySelectorAll( '.slide' );
+	for( var i = 0; i < slides.length; i++ ) {
+		slides[ i ].setAttribute( 'data-y', 2000 * i );
+	}
+
 	impress().init();
 	var beeSwarmContainer = document.querySelector( '.swarm-b' );
 	var beeSwarm = new BeeSwarm( beeSwarmContainer, 1000, 600 );
-	beeSwarm.start();
+//	beeSwarm.start();
 	var rootElement = document.getElementById( "impress" );
 	rootElement.addEventListener( "impress:stepenter", function() {
 		var currentStep = document.querySelector( ".present" );
